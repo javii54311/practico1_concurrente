3e0e27cc26764ad5306db8c28f49d0d1b3b254e4
Francisco Javier Vasquez <javi54311@gmail.com>
2024-04-11 23:51:16 -0300

    a
    elimine un getter, se agrego chequeo en procesarReservaConfirmada

--- src/Avion.java
+++ src/Avion.java
@@ -134,13 +134,6 @@ public class Avion {
 
     }
 
-
-    public RegistroReservas getRegistroReservas() {
-
-        return registroReservas;
-    }
-
-
     public void procesarReservasPendientes() {
         Random random = new Random();
 
@@ -183,22 +176,27 @@ public class Avion {
             int indiceAleatorio = random.nextInt(registroReservas.getReservasConfirmadas().size());
             Reserva reserva = registroReservas.getReservasConfirmadas().get(indiceAleatorio);
 
-            // Intentar cancelar la reserva con un 10% de probabilidad
-            if (random.nextDouble() < 0.1) {
-                // Marcar el asiento asociado como DESCARTADO
-                System.out.println("Reserva " + reserva.getFila() + " "+ reserva.getColumna() + " cancelada");
-                reserva.getAsiento().setEstado(EstadoAsiento.DESCARTADO);
-                // Eliminar la reserva de la lista de confirmadas
-                registroReservas.eliminarReservaConfirmada(reserva);
-                // Agregar la reserva a la lista de canceladas
-                registroReservas.agregarReservaCancelada(reserva);
+            if(reserva.getCheck()){
+                System.out.println("La reserva " + reserva.getFila()+ " " + reserva.getColumna()+  " estaba checkeada");
             }
             else{
-                System.out.println("Reserva "+reserva.getFila() + " " + reserva.getColumna()+" no cancelada");
-                System.out.println("Se la marca como chequeada");
-                reserva.setCheck(true);
-
+                // Intentar cancelar la reserva con un 10% de probabilidad
+                if (random.nextDouble() < 0.1) {
+                    // Marcar el asiento asociado como DESCARTADO
+                    System.out.println("Reserva " + reserva.getFila() + " "+ reserva.getColumna() + " cancelada");
+                    reserva.getAsiento().setEstado(EstadoAsiento.DESCARTADO);
+                    // Eliminar la reserva de la lista de confirmadas
+                    registroReservas.eliminarReservaConfirmada(reserva);
+                    // Agregar la reserva a la lista de canceladas
+                    registroReservas.agregarReservaCancelada(reserva);
+                }
+                else{
+                    System.out.println("Reserva "+reserva.getFila() + " " + reserva.getColumna()+" no cancelada");
+                    System.out.println("Se la marca como chequeada");
+                    reserva.setCheck(true);
+                }
             }
+
         }
         else{
             System.out.println("No hay reservas confirmadas");
